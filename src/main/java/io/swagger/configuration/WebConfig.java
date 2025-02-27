8bada4d70cf6f5cd4ee9c5df8a91523002b7db70
Zhumuyoutong <xs2415@columbia.edu>
2025-02-27 15:15:53 -0500

    config allowedoriginpattern, update jar file

--- src/main/java/io/swagger/configuration/WebConfig.java
+++ src/main/java/io/swagger/configuration/WebConfig.java
@@ -5,16 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.swagger.controller.PolicyController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+  private static final Logger logger = LoggerFactory.getLogger(PolicyController.class);
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                logger.info("Configuring CORS mappings");
                 registry.addMapping("/api/**")
-                        .allowedOriginPatterns("*") // You can specify allowed origins here
+                        .allowedOriginPatterns("http://localhost:3000") // You can specify allowed origins here
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
